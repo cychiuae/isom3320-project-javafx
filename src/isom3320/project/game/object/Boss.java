8620224999612acd6d5e62a948063271a1a0ec52
cychiuae <cychiuae@connect.ust.hk>
2015-01-30 18:18:26 +0800

    added boss

---
+++ src/isom3320/project/game/object/Boss.java
@@ -0,0 +1,39 @@
+package isom3320.project.game.object;
+
+import isom3320.project.game.Map.Map;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+
+//Billy you do la
+public class Boss extends Enemy {
+
+	public Boss(Map map) {
+		super(map);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyCode keyCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyCode keyCode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
