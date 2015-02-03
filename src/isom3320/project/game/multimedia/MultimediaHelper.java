dfd0988a49ea3257f5ae0f9038d3b112c45e7002
kevingok <kevingok@hotmail.com>
2015-02-03 11:52:51 +0800

    Javadoc(MultimediaHelper)

--- src/isom3320/project/game/multimedia/MultimediaHelper.java
+++ src/isom3320/project/game/multimedia/MultimediaHelper.java
@@ -6,9 +6,21 @@ import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.media.Media;
 
+/**
+ * Class MultimediaHelper assists for getting image or music file.
+ * @author kevingok
+ *
+ */
 public class MultimediaHelper {
+	
+	/**Defines directory of media resources*/
 	public static final String resourcesDir = "Resources/";
 	
+	/**
+	 * Returns an Image object that can then be painted on the screen.
+	 * @param imageName		the filename of the image
+	 * @return				image at specified path
+	 */
 	public static Image getImageByName(String imageName) {
 		Image image = null;
 		File imageFile = new File(resourcesDir + imageName);
@@ -23,6 +35,11 @@ public class MultimediaHelper {
 		return image;
 	}
 	
+	/**
+	 * Returns a music object that can then be played during gameplay.
+	 * @param musicName		the filename of the music
+	 * @return				music(mp3) at specified path
+	 */
 	public static Media getMusicByName(String musicName) {
 		Media music = null;
 		
@@ -35,6 +52,15 @@ public class MultimediaHelper {
 		return music;
 	}
 	
+	/**
+	 * Returns writable image object that can then be played during gameplay.
+	 * @param image			the image object
+	 * @param x				x-coordinate
+	 * @param y				y-coordinate
+	 * @param width			Width of the window
+	 * @param height		Height of the window
+	 * @return				Sub image of the image object			
+	 */
 	public static WritableImage getSubImage(Image image, int x, int y, int width, int height) {
 		return new WritableImage(image.getPixelReader(), x, y, width, height);
 	}
